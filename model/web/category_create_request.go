8964be94cbdb8a762b6884365b12e2db52919c2c
portfolioaidiladam <portfolioaidiladam@gmail.com>
2025-07-11 22:31:48 +0700

    update

--- model/web/category_create_request.go
+++ model/web/category_create_request.go
@@ -2,7 +2,7 @@ package web
 
 // CategoryCreateRequest adalah struct untuk request pembuatan kategori baru.
 type CategoryCreateRequest struct {
-	// Name adalah nama kategori yang akan dibuat.
+	// Name adalah nama kategori yang akan dibuat
 	// Validasi: required (wajib diisi), min=1 (minimal 1 karakter), max=100 (maksimal 100 karakter)
 	Name string `validate:"required,min=1,max=100" json:"name"`
 }
