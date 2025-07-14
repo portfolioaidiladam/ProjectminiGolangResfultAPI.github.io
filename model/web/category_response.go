f6e0cd274de5051e1f4ea68d2e8bc1e1ecf57040
portfolioaidiladam <portfolioaidiladam@gmail.com>
2025-07-14 23:05:24 +0700

    update

--- model/web/category_response.go
+++ model/web/category_response.go
@@ -3,7 +3,7 @@ package web
 // CategoryResponse adalah struct untuk response data kategori.
 type CategoryResponse struct {
 	// Id adalah identitas unik kategori
-	Id   int    `json:"id"`
-	// Name adalah nama kategori
+	Id int `json:"id"`
+	// Name adalah nama kategori.
 	Name string `json:"name"`
 }
